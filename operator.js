2d4ec83393a73a02b4a0c6858d0b7897eab584cc
Ayush Yadav <infoayushyadav96@gmail.com>
2023-02-13 10:58:57 +0530

    first commit

---
+++ operator.js
@@ -0,0 +1,19 @@
+// alert("Hello I am from External Js");
+// operator and operator
+var num1=20;
+num1=-num1;
+// num1-=num1;
+
+console.log(num1);
+
+// Arithmatic operations
+var x=20,y=30;
+console.log("Addition of X & Y is :"+(x+y));
+console.log("Subtraction of X & Y is :"+(x-y));
+console.log("Multiplication of X & Y is :"+(x*y));
+console.log("Division of X & Y is :"+(x/y));
+
+// concatination
+var firstName="Ayush" ,lastName="Yadav";
+console.log(firstName+" " +lastName);
+console.log(`Your First Name is ${firstName} and Your Last Name is ${lastName} Hence the full Name is ${firstName} ${lastName}`);
